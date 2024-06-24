fe7e6e509f056b56c54b722cec5ba9f480a6dd3c
Nicole Rojas <nicole.eli.valdes@gmail.com>
2024-06-24 12:11:09 +0000

    Add crispy forms to login, logout and signup.html

--- Elvira_bookings/settings.py
+++ Elvira_bookings/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'cloudinary',
     'cloudinary_storage',
     'django_summernote',
@@ -57,6 +59,25 @@ INSTALLED_APPS = [
     'reviews',
 
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+DJRICHTEXTFIELD_CONFIG = {
+    'js': ['//cdn.ckeditor.com/4.14.0/standard/ckeditor.js'],
+    'init_template': 'djrichtextfield/init/ckeditor.js',
+    'settings': {
+        'toolbar': [
+            ['Format', 'Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList'], ['Undo', 'Redo'],
+            ['Maximize']
+        ],
+        'format_tags': 'p;h1;h2;h3'
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
