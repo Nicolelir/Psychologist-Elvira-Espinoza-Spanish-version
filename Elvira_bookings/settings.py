3d5935685803425095f7460ed671b233a1701d4b
Nicole Rojas <nicole.eli.valdes@gmail.com>
2024-06-24 15:29:14 +0000

    Change templates to Spanish Language

--- Elvira_bookings/settings.py
+++ Elvira_bookings/settings.py
@@ -149,7 +149,12 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
+
+LANGUAGES = [
+    ('es', 'Español'),
+]
+
 
 TIME_ZONE = 'UTC'
 
