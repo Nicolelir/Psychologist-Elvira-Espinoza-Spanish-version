2f188e43f409ac87f8c0b91782d786614257bec4
Nicole Rojas <nicole.eli.valdes@gmail.com>
2025-01-05 17:43:01 +0000

    Add padding to text in template

--- Elvira_bookings/settings.py
+++ Elvira_bookings/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = BASE_DIR / 'templates'
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7j9g2q0gc8@jx5%eofg(zd7*tib-(^o=^68=orhhj-yn+6n+g='
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
