7673fb9bfaee128bcc5d6c6d171944513626d1a7
Nicole Rojas <nicole.eli.valdes@gmail.com>
2025-01-06 02:20:39 +0000

    deployment settings error heroku

--- Elvira_bookings/settings.py
+++ Elvira_bookings/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = BASE_DIR / 'templates'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['.gitpod.io', '.herokuapp.com', '8000-nicolelir-psychologiste-4cknfcrju1j.ws.codeinstitute-ide.net']
 CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io', 'https://8000-nicolelir-psychologiste-4cknfcrju1j.ws.codeinstitute-ide.net'] 
 
